908e12221dfa219f0bc496564c4b2244e3725114
Andris011 <andris.febert111@gmail.com>
2026-02-09 21:26:29 +0100

    Create Menu

---
+++ MainMenu.cs
@@ -0,0 +1,28 @@
+﻿namespace IKT_bunbarlang;
+
+public class MainMenu
+{   
+    public static void Show()
+    {
+        Player player = new Player(2001911);
+
+        string[] menuOpciok = { "Lóverseny", "Rulett", "Black Jack" };
+
+        int valasztas = Menu.ValasztasLista(menuOpciok.ToList());
+
+        switch (valasztas)
+        {
+            case 0:
+                Loverseny.Loverseny.Play(player);
+                break;
+
+            case 1:
+                Rulette.Rulette.Play(player);
+                break;
+
+            case 2:
+                Black_Jack.BlackJack.Play(player);
+                break;
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,48 @@
+﻿namespace IKT_bunbarlang;
+
+public class Menu
+{
+    public static int ValasztasLista(List<string> opciok)
+    {
+        int valasztas = 0;
+        bool vege = false;
+
+        while (!vege)
+        {
+            for (int i = 0; i < opciok.Count(); i++)
+            {
+                string before = valasztas == i ? "> " : "  ";
+                int halfWidth = (Console.WindowWidth - opciok[i].Length) / 2;
+
+                Console.WriteLine($"{new string(' ', halfWidth)}{before}{opciok[i]}");
+            }
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow:
+                    valasztas = Math.Max(0, valasztas - 1);
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    valasztas = Math.Min(opciok.Count() - 1, valasztas + 1);
+                    break;
+
+                case ConsoleKey.Enter:
+                    vege = true;
+                    break;
+            }
+
+            Console.Write($"\r\x1B[{opciok.Count()}A");
+        }
+
+        for (int i = 0; i < opciok.Count(); i++)
+        {
+            Console.Write($"\r\x1B[2K\n");
+        }
+
+        Console.Write($"\r\x1B[{opciok.Count()}A");
+        
+
+        return valasztas;
+    }
+}
