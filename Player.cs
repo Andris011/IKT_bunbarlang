f25fcf4197b68ab4ac7f618c295b8cb0e59d759d
Andris011 <andris.febert111@gmail.com>
2026-02-09 21:04:54 +0100

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿namespace IKT_bunbarlang;
+
+public class Player
+{
+    private int zsetonok;
+
+    public int Zsetonok
+    {
+        get => zsetonok;
+        set => zsetonok = value;
+    }
+
+    public Player(int zsetonok)
+    {
+        this.zsetonok = zsetonok;
+    }
+
+    public override string ToString()
+    {
+        return $"Játékos(zsetonok={zsetonok})";
+    }
+}
