86e5f5f8a1404f98067201e8fe70c3a565f7ba6a
Csidom <csikszi.dome@gmail.com>
2026-02-10 19:20:22 +0100

    blackjack kártyapakli kész

--- Black_Jack/BlackJack.cs
+++ Black_Jack/BlackJack.cs
@@ -4,6 +4,39 @@ public class BlackJack
 {
     public static void Play(Player player)
     {
-        
+
+        List<Kartya> pakli = new List<Kartya>();
+
+        string[] szinek = {"Pikk", "Treff", "Kőr", "Káró" };
+        string[] nevek = {"2","3","4", "5", "6", "7", "8", "9", "10", "Bubi", "Dáma", "Király", "Ász" };
+
+        foreach (string szin in szinek)
+        {
+            foreach (string nev in nevek)
+            {
+                int ertek = 0;
+
+                if (nev == "Bubi" || nev == "Dáma" || nev == "Király")
+                {
+                    ertek = 10;
+                }
+                else if (nev == "Ász")
+                {
+                    ertek = 11;
+                }
+                else
+                {
+                    ertek = int.Parse(nev);
+                }
+
+                pakli.Add(new Kartya(nev, szin, ertek));
+            }
+        }
+
+        foreach (Kartya kartya in pakli)
+        {
+            Console.WriteLine(kartya);
+        }
+
     }
 }
---
+++ Black_Jack/Kartya.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKT_bunbarlang.Black_Jack
+{
+    internal class Kartya
+    {
+        private string nev;
+        private string szin;
+        private int ertek;
+
+        public Kartya(string nev, string szin, int ertek)
+        {
+            this.nev = nev;
+            this.szin = szin;
+            this.ertek = ertek;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Szin { get => szin; set => szin = value; }
+        public int Ertek { get => ertek; set => ertek = value; }
+
+        public override string ToString()
+        {
+            return $"{this.szin} {this.nev}";
+        }
+    }
+}
