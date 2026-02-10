d76c0004a269239d3b8014b758f36f78ed34594b
Andris011 <andris.febert111@gmail.com>
2026-02-10 14:32:20 +0100

    Create Lo.cs

---
+++ Loverseny/Lo.cs
@@ -0,0 +1,38 @@
+﻿namespace IKT_bunbarlang.Loverseny;
+
+public class Lo
+{
+    private string _name;
+    private string _age;
+    private string _rider;
+
+    public Lo(string name, string age, string rider)
+    {
+        _name = name;
+        _age = age;
+        _rider = rider;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Age
+    {
+        get => _age;
+        set => _age = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Rider
+    {
+        get => _rider;
+        set => _rider = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    override public string ToString()
+    {
+        return $"Név: {_name}, kor: {_age}, zsoké: {_rider}";
+    }
+}
--- Loverseny/Loverseny.cs
+++ Loverseny/Loverseny.cs
@@ -2,8 +2,30 @@
 
 public class Loverseny
 {
+    private static List<Lo> lovak = new List<Lo>();
+
+    public static void LovakKiirasa()
+    {
+        lovak.Add(new Lo("Anamoe", "2018", "James McDonald"));
+        lovak.Add(new Lo("Al Riffa", "2019", "Mark Zahra"));
+        lovak.Add(new Lo("Buckaroo", "2018", "Craig Williams"));
+        lovak.Add(new Lo("Arapho", "2016", "Rachel King"));
+        lovak.Add(new Lo("Vauban", "2017", "Blake Shinn"));
+        lovak.Add(new Lo("Chevalier Rose", "2017", "Damian Lane"));
+        lovak.Add(new Lo("Prasage Nocturne", "2019", "Stephane Pasquier"));
+        lovak.Add(new Lo("Meydaan", "2020", "James McDonald"));
+        lovak.Add(new Lo("Absurde", "2017", "Kerrin McEvoy"));
+        lovak.Add(new Lo("Flatten The Curve", "2018", "Thore Hammer Hansen"));
+
+        foreach (var lo in lovak)
+        {
+            Console.WriteLine(lo);
+        }
+    }
+    
+    
     public static void Play(Player player)
     {
-        
+        LovakKiirasa();
     }
 }
